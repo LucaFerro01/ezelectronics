714a0be131cdf5c9ca2ac0b84bd41ef613a9b5a4
ALESSANDRA PENZA s331062 <s331062@studenti.polito.it>
2024-05-31 12:54:43 +0200

    ready to merge

--- code/server/src/db/cart_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Cart (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    customer text NOT NULL,
-    paid INTEGER DEFAULT 0,
-    paymentDate DATE,
-    total DECIMAL(10, 2) DEFAULT 0.0,
-    products TEXT,
-    FOREIGN KEY (customer) REFERENCES User(id)
-);
